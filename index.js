24803e4a8657d2ae6cea1223164dbe71f215c157
Alex Sytar <alex.sitar.work@gmail.com>
2018-05-04 14:58:42 +0300

    second try

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
 const Express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const axiosRetry = require('axios-retry');
 
 const app = new Express();
 const port = 3030;
 
 app.use(bodyParser.json());
 
-axiosRetry(axios, {
-  retries: 1,
-  delay(retryCount) {
-    console.log('retryCount -log- =>', retryCount);
-    return 100;
-  },
-  retryCondition() {
-    return true;
-  }
-});
-
 const getReqConf = (userID, endpoint, updates) => ({
   url: `${endpoint.url}/${userID}`,
   method: endpoint.method,
   data: { ...updates },
+  validateStatus: () => true,
 });
 
 app.post('/batch', (req, res) => {
   const responsesArr = {};
-  axios.all(
-    req.body.userIDs.map(userID => {
-      return axios(getReqConf(userID, req.body.endpoint, req.body.updates))
-        .then(response => {
-          responsesArr[userID] = {
-            ...response.data,
-            statusCode: response.status,
-          };
-        })
-        .catch(err => {
-          responsesArr[userID] = {
-            status: error.response.status,
-            statusCode: error.response.statusCode,
-          };
-        })
-    })
-  ).then((resp) => {
-    res.status(200).send(responsesArr);
-  });
+  const functionsArr = req.body.userIDs.map(userID => (async () => {
+    const reqConf = getReqConf(userID, req.body.endpoint, req.body.updates);
+    let data = null;
+    try {
+      data = await axios(reqConf);
+
+      if (data.status !== 200) {
+        data = await axios(reqConf);
+
+        if (data.status !== 200) {
+          throw data;
+        }
+      }
+
+      return {
+        ...data.data,
+      };
+    } catch (e) {
+      return {
+        status: e.status,
+        statusText: e.statusText,
+      };
+    }
+  }));
+
+  Promise.all(functionsArr.map(func => func())).then((results) => {
+    res.status(200).send(results);
+  })
 });
 
 app.listen(port, (error) => {
